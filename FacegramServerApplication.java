f7ff2dfe277071ca94e697a98af428488984c205
benkedan <65911902+benkedan@users.noreply.github.com>
2020-05-27 18:52:26 +0200

    Add files via upload

---
+++ FacegramServerApplication.java
@@ -0,0 +1,18 @@
+package server;
+
+public class FacegramServerApplication {
+
+	FaceGramServer server;
+	
+	public FacegramServerApplication(int port, String userdata) {
+		
+	}
+	
+	public void login(String username, String password, String ip) {
+		
+	}
+	
+	public void register(String name, String lastname, String coordinates, String username, String password, String ip) {
+		
+	}
+}
